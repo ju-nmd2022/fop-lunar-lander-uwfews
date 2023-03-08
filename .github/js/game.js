b30ad3b71309f9a21313ce5485c6c0e1dacad2a7
yousef <nayo21da@student.ju.se>
2023-03-08 01:05:41 +0100

    with environment

--- .github/js/game.js
+++ .github/js/game.js
@@ -4,7 +4,7 @@ function setup() {
 
 //Draw Rocket//
 
-function drawrocket(positionX, positionY) {
+function drawRocket(positionX, positionY) {
   push();
   translate(positionX, positionY);
   noFill();
@@ -16,5 +16,29 @@ function drawrocket(positionX, positionY) {
 
 function draw() {
   background(20);
-  drawrocket(150, 125);
+  drawRocket(150, 125);
 }
+
+//Draw Environment//
+
+function drawEnvironment() {
+  background(20);
+  push();
+  noFill();
+  stroke(220);
+  ellipse(width / 2 - 40, height - 20, width / 8);
+  ellipse(width / 2 + 30, height - 10, width / 8);
+  ellipse(width / 2 + 20, height - 60, width / 8);
+  ellipse(width / 2 - 30, height - 55, width / 28);
+  ellipse(width / 2 + 70, height - 15, width / 28);
+  strokeWeight(2.5);
+  ellipse(width / 2, height - 20, width / 2);
+  pop();
+}
+
+function draw() {
+  drawEnvironment();
+  drawrocket(155, 125);
+}
+
+//Write Gravity logic//
